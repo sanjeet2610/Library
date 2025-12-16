19faaec8866c2705e8138b28dc376dba1b39f26f
sanjeet2610 <195545915+sanjeet2610@users.noreply.github.com>
2025-12-16 15:42:55 +0000

    Replace constructor with class

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 const display = document.querySelector(".display");
